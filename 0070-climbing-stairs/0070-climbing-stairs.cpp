d656728ee618c994f9e8daddbb2a0c684a8e2546
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-07-09 11:59:50 +0530

    Time: 0 ms (100%), Space: 8.6 MB (36.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-unordered_map<int,int>dp;
-int fun(int i,int n){
-    if(i==n)return 1;
-    if(i>n)return 0;
-if(dp.find(i)!=dp.end())return dp[i];
-    int a=fun(i+1,n);
-    int b=fun(i+2,n);
-dp[i]=a+b;
-    return a+b;
-}
+//tabulation
     int climbStairs(int n) {
-        return fun(0,n);
+        vector<int>dp(n+2);
+        dp[n]=1;
+        dp[n+1]=0;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=dp[i+1]+dp[i+2];
+        }
+return dp[0];
     }
 };

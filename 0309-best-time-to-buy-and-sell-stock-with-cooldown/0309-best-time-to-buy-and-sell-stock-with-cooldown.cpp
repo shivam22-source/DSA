da270b2daea9d394a50988df133a2494ba585827
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-07-16 13:17:46 +0530

    Time: 0 ms (100%), Space: 16.1 MB (64.35%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         int k = 2;
+        int n = prices.size();
+        vector<vector<int>>dp(n+2,vector<int>(3,0));
+       
+        for(int i=n-1;i>=0;i--){
+            for(int j=2;j>=0;j--){
+                if(j==0)continue;
+                if(j==2){
+                    dp[i][j]=max(-prices[i]+dp[i+1][j-1],dp[i+1][j]);
+                }
+                else {
+                     dp[i][j]=max(prices[i]+dp[i+2][2],dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][2];
+    }
+};

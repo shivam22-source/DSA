e61c8fed657b7c6c1205bd3e7bb84a15ceedbcf7
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-07-18 13:47:51 +0530

    Time: 84 ms (5.03%), Space: 37.4 MB (5.15%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,10 +4,15 @@ public:
         int n=strs.size();
         unordered_map<string,vector<string>>mp;
        for(auto it:strs){
-        string temp=it;
-        sort(temp.begin(),temp.end());
-        mp[temp].push_back(it);
-       } //["aet"]->{["eat"],["ate"],["tea"]}
+       vector<int>freq(26,0);
+       for(auto ch:it)freq[ch-'a']++;
+
+       string token; //use as a key
+       for(auto it:freq){
+        token+=to_string(it)+"#";
+       }
+        mp[token].push_back(it);
+       } //"#1011##1100"->{["eat"],["ate"],["tea"]}
 vector<vector<string>>res;
        for(auto it:mp){
         res.push_back(it.second); //["eat"],["ate"],["tea"]
